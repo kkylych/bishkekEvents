b9016f751a91d0609ee9ad9176e5fe5f89bb9695
kminbaeva <mederbekova0904@gmail.com>
2024-11-16 16:55:54 +0600

    new config

--- src/main/java/com/bishevents/exception/CustomExceptionHandler.java
+++ src/main/java/com/bishevents/exception/CustomExceptionHandler.java
@@ -4,17 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseBody
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
         return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
